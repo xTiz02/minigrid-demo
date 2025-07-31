934636935d49ba354e150609e2d8ffb9da64470e
pierord6201 <pierord6201@gmail.com>
2025-07-31 14:05:25 -0500

    .2

---
+++ main3.py
@@ -0,0 +1,22 @@
+import minigrid
+from minigrid.manual_control import ManualControl
+import gymnasium as gym
+
+class ManualControlWithObs(ManualControl):
+    def step(self, action):
+        print("Action: ",action)
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        print("Obs:", obs)  # 👈 Aquí se imprime la observación en cada paso
+        print("Reward:",reward)
+        if terminated or truncated:
+            print("Episode done. Resetting environment.")
+            self.reset()
+
+# Crear el entorno
+env = gym.make("MiniGrid-Empty-16x16-v0", render_mode="human")
+
+# Usar la clase personalizada
+manual_control = ManualControlWithObs(env, seed=42)
+manual_control.start()
+
+env.close()
